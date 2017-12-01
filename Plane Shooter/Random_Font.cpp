43c608cd7ffe54e533c72834fd1721e08412ced0
vfjpl <vfjpl1@gmail.com>
2017-12-01 22:38:17 +0100

    update

--- Plane Shooter/Random_Font.cpp
+++ Plane Shooter/Random_Font.cpp
@@ -5,9 +5,7 @@ ALLEGRO_FONT* Random_Font()
     ALLEGRO_FONT* font1 = al_load_font("fonts/font-1.ttf", 22, 0);
 	ALLEGRO_FONT* font2 = al_load_font("fonts/font-2.ttf", 22, 0);
 
-    bool temp = rand()%2;
-
-    switch(temp)
+    switch( rand()%2 )
     {
     case 0:
         {
